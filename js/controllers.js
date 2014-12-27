e8bcba5634a54607acc2e4694116efa35a3b0eee
andela-ladenusi <ladi.adenusi@andela.co>
2014-12-27 10:12:20 +0100

    Chnages on controllers.js...added factory

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,30 @@
 
-var portfolio = angular.module('portfolio', []);
+var app = angular.module('portfolio', []);
 
-portfolio.controller('ReposCtrl', function($scope, $http) {
+app.factory('Utils', function($q) {
+  return {
+    isImage: function(src) {
+    
+      var deferred = $q.defer();
+  
+      var image = new Image();
+      image.onerror = function() {
+        deferred.resolve(false);
+        return false;
+      };
+      image.onload = function() {
+        deferred.resolve(true);
+        return true;
+      };
+
+      image.src = src;
+
+      return deferred.promise;
+    }
+  };
+}); // end app.factory
+
+app.controller('ReposCtrl', function($scope, $http, Utils) {
   
   $scope.show = false;
 
@@ -17,18 +40,29 @@ portfolio.controller('ReposCtrl', function($scope, $http) {
     success(function(data, status) {
       if(status === 200) {
         $scope.repos = data;
-        console.log(data);
+        
+        //loop through all the repos and add 'liveViewUrl' and 'imageSrc' properties 
         for(var i = 0; i < $scope.repos.length; i++) {
           var liveViewUrl = 'http://' + $scope.query + '.github.io/' + $scope.repos[i].name,
-              imageUrl = 'images/' + $scope.repos[i].name + '.png';
+              imageUrl = new Array(2);
+
+          //imageUrl
+          imageUrl[0] = 'images/' + $scope.repos[i].name + '.png';
+
+          //default image url
+          imageUrl[1] = 'images/placeholder.png';
+          
+          // $scope.repos[i].imageSrc = (Utils.isImage(imageUrl[0])) ? imageUrl[0] : imageUrl[1];
+
+
+          //check if the a repo has gh-pages, if not, set live view to not available
           if($scope.repos[i].has_pages === true) {
             $scope.repos[i].liveView = {url: liveViewUrl, text: "click for live view!"};
           }
           else {
             $scope.repos[i].liveView = {url: "", text: "not available"};
           }
-          $scope.repos[i].imageSrc = imageUrl;
-        }
+        } // end for loop
       }
     }).
     error(function(data, status) {
@@ -39,5 +73,19 @@ portfolio.controller('ReposCtrl', function($scope, $http) {
         alert("Oops! We are sorry!\nThere was an error with the request.");
       }
     }); // end $http.get
-  }
-}); // end ReposCtrl
+
+  }; // end $scope.generatePortfolio
+
+}); // end app.controller
+
+
+
+
+
+
+
+
+
+
+
+
